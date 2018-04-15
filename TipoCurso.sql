60e33c187d6bcf9c01b0a5763c5c3ac7b629ca0a
Fábio Gaspar <fabiodanielreis@hotmail.com>
2018-04-15 18:30:40 +0100

    Added *TipoCurso*

---
+++ TipoCurso.sql
@@ -0,0 +1,8 @@
+/*
+    TipoCurso
+*/
+
+insert into TipoCurso (nome) values ("Mestrado");
+insert into TipoCurso (nome) values ("Mestrado Integrado");
+insert into TipoCurso (nome) values ("Licenciatura");
+insert into TipoCurso (nome) values ("Doutoramento");
