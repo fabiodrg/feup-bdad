d4983f72d4d5f8ee5df4484faf7fd90dec9b0678
Fábio Gaspar <fabiodanielreis@hotmail.com>
2018-04-15 18:43:43 +0100

    Added *TipoAula*

---
+++ TipoAula.sql
@@ -0,0 +1,7 @@
+/*
+    TipoAula
+*/
+
+insert into TipoAula (nome) values ("Teória");
+insert into TipoAula (nome) values ("Teórica e Prática");
+insert into TipoAula (nome) values ("Laboratorial");
