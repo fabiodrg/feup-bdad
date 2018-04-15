34dee8fcb8c74de38a8268d625ec27011b69d069
Fábio Gaspar <fabiodanielreis@hotmail.com>
2018-04-15 18:30:47 +0100

    Added *Departamento*

---
+++ Departamento.sql
@@ -0,0 +1,11 @@
+/*
+    Departamento
+*/
+
+insert into Departamento (nome) values ("dept1");
+insert into Departamento (nome) values ("dept2");
+insert into Departamento (nome) values ("dept3");
+insert into Departamento (nome) values ("dept4");
+insert into Departamento (nome) values ("dept5");
+insert into Departamento (nome) values ("dept6");
+insert into Departamento (nome) values ("dept7");
