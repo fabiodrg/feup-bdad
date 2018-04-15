a84db396d33f81a8db49cec1786024a5aa6b793b
Fábio Gaspar <fabiodanielreis@hotmail.com>
2018-04-15 18:43:29 +0100

    Added *TipoSala*

---
+++ Data/TipoSala.sql
@@ -0,0 +1,10 @@
+/*
+    TipoSala
+*/
+
+insert into TipoSala (nome) values ("Gabinete");
+insert into TipoSala (nome) values ("Sala de Computadores");
+insert into TipoSala (nome) values ("Laboratorio");
+insert into TipoSala (nome) values ("Anfiteatro");
+insert into TipoSala (nome) values ("Auditorio");
+insert into TipoSala (nome) values ("Normal");
