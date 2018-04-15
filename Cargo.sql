a7016d92b4f7da45da06ed89b90d4146979310d4
Fábio Gaspar <fabiodanielreis@hotmail.com>
2018-04-15 18:24:11 +0100

    Added *Cargo* data

---
+++ Cargo.sql
@@ -0,0 +1,7 @@
+/*
+    Cargo
+*/
+insert into Cargo (nome) values ("Diretor de Curso");
+insert into Cargo (nome) values ("Professor");
+insert into Cargo (nome) values ("cargo1");
+insert into Cargo (nome) values ("cargo2");
