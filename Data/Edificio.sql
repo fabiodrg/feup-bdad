ceeeac9aa5210f18dd81446a4d491dcc6669313a
Fábio Gaspar <fabiodanielreis@hotmail.com>
2018-04-15 18:43:18 +0100

    Added *Edificio*

---
+++ Data/Edificio.sql
@@ -0,0 +1,15 @@
+insert into Edificio (nome, sigla, dept) values ('curae', 'JJSC', 4);
+insert into Edificio (nome, sigla, dept) values ('proin', 'SFME', 3);
+insert into Edificio (nome, sigla, dept) values ('mi', 'UADT', 5);
+insert into Edificio (nome, sigla, dept) values ('a nibh in', 'EFYK', 3);
+insert into Edificio (nome, sigla, dept) values ('id sapien', 'SRFF', 3);
+insert into Edificio (nome, sigla, dept) values ('diam', 'TJXO', 3);
+insert into Edificio (nome, sigla, dept) values ('odio consequat', 'SVQG', 6);
+insert into Edificio (nome, sigla, dept) values ('amet', 'IVIB', 7);
+insert into Edificio (nome, sigla, dept) values ('arcu libero', 'VSWS', 6);
+insert into Edificio (nome, sigla, dept) values ('ut at dolor', 'QFRO', 5);
+insert into Edificio (nome, sigla, dept) values ('convallis', 'TMKV', 1);
+insert into Edificio (nome, sigla, dept) values ('ridiculus mus', 'CMLC', 4);
+insert into Edificio (nome, sigla, dept) values ('dolor', 'PLHQ', 4);
+insert into Edificio (nome, sigla, dept) values ('tellus nulla', 'QTJM', 3);
+insert into Edificio (nome, sigla, dept) values ('at', 'JEGD', 4);
