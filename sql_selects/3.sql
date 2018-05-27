ce4d7b6291826b1fb8bd8bc6f996404df3f40473
Fábio Gaspar <fabiodanielreis@hotmail.com>
2018-05-27 23:14:52 +0100

    Cleanup

--- sql_selects/3.sql
+++ sql_selects/3.sql
@@ -2,13 +2,11 @@
 .headers	on
 .nullvalue	NULL
 
--- same UC, different grades
---insert into Classificacao values(218, 173, 15);
---insert into Classificacao values(218, 685, 5);
-
 SELECT idUC, ano, semestre, nomeUC, anoLetivo, nota
-FROM (SELECT max(nota), idUC, ano, semestre, nomeUC, anoLetivo, nota
-FROM Estudante NATURAL JOIN Classificacao NATURAL JOIN OcorrenciaUC NATURAL JOIN UC
-WHERE idEstudante = 218
-GROUP BY idUC)
+FROM (
+    SELECT max(nota), idUC, ano, semestre, nomeUC, anoLetivo, nota
+    FROM Estudante NATURAL JOIN Classificacao NATURAL JOIN OcorrenciaUC NATURAL JOIN UC 
+    WHERE idEstudante = 218  
+    GROUP BY idUC
+)
 ORDER BY ano, semestre, nomeUC;
