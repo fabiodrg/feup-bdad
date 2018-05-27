177c150cbbbce3f33448714ef629cf9f5d3fa183
Fábio Gaspar <fabiodanielreis@hotmail.com>
2018-05-27 22:18:34 +0100

    Added reference to Edificio

--- sql_selects/1.sql
+++ sql_selects/1.sql
@@ -2,12 +2,12 @@
 .headers	on
 .nullvalue	NULL
 
-SELECT numeroSala AS "No. Sala", nomeTipoSala
-FROM Sala NATURAL JOIN TipoSala
+SELECT nomeEdificio AS "Edificio", numeroSala AS "No. Sala", nomeTipoSala AS "Tipo"
+FROM Sala NATURAL JOIN TipoSala NATURAL JOIN Edificio
 WHERE idSala NOT IN (
     SELECT idSala
     FROM Sala
     NATURAL JOIN Aula
     WHERE diaSemana == 0 AND strftime('%H', hora) == '10'
 ) AND idTipoSala <> 1 AND idTipoSala <> 5
-ORDER BY numeroSala;
+ORDER BY nomeEdificio, numeroSala;
